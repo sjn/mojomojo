531358871e067bf4b5693db3fc664223d0c459c8
mojomojo <mojomojo@badius.kaizendo.org>
2010-06-14 19:40:54 +0200

    Updated catalyst scripts

--- script/mojomojo_create.pl
+++ script/mojomojo_create.pl
@@ -2,37 +2,9 @@
 
 use strict;
 use warnings;
-use Getopt::Long;
-use Pod::Usage;
-eval "use Catalyst::Helper;";
 
-if ($@) {
-  die <<END;
-To use the Catalyst development tools including catalyst.pl and the
-generated script/myapp_create.pl you need Catalyst::Helper, which is
-part of the Catalyst-Devel distribution. Please install this via a
-vendor package or by running one of -
-
-  perl -MCPAN -e 'install Catalyst::Devel'
-  perl -MCPANPLUS -e 'install Catalyst::Devel'
-END
-}
-
-my $force = 0;
-my $mech  = 0;
-my $help  = 0;
-
-GetOptions(
-    'nonew|force'    => \$force,
-    'mech|mechanize' => \$mech,
-    'help|?'         => \$help
- );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-my $helper = Catalyst::Helper->new( { '.newfiles' => !$force, mech => $mech } );
-
-pod2usage(1) unless $helper->mk_component( 'MojoMojo', @ARGV );
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('MojoMojo', 'Create');
 
 1;
 
@@ -45,9 +17,9 @@ mojomojo_create.pl - Create a new Catalyst Component
 mojomojo_create.pl [options] model|view|controller name [helper] [options]
 
  Options:
-   -force        don't create a .new file where a file to be created exists
-   -mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
-   -help         display this help and exits
+   --force        don't create a .new file where a file to be created exists
+   --mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
+   --help         display this help and exits
 
  Examples:
    mojomojo_create.pl controller My::Controller
@@ -77,12 +49,9 @@ This behavior can be suppressed with the C<-force> option.
 
 Catalyst Contributors, see Catalyst.pm
 
-=head1 LICENSE
-
-You may distribute this code under the same terms as Perl itself.
-
 =head1 COPYRIGHT
 
-Please refer to Catalyst.pm for copyright details.
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
